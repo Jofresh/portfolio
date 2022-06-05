dffdf9144fc65e91f0cc29020d0e9fc1bbd89cd1
Geoffray Gaborit <gaborit.geoffray@icloud.com>
2022-06-05 18:58:38 +0200

    must fix input width

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("hello");
+const data = [
+    { command: "about", output: "Geoffray, 21 ans"},
+    { command: "projects", output: "Projet Périscolaire, Spring Boot & Svelte"},
+    { command: "experience", output: "Analys Informatique 2020 - 2022"},
+    { command: "education", output: "DUT Info, L3 Info + MIAGE"},
+    { command: "clear" }
+]
+
+const shellEl = document.querySelector("#shell")
+const inputEl = document.querySelector("#command")
+const outputsEl = document.querySelector("#outputs")
+
+inputEl.focus()
+
+inputEl.onkeypress = ({keyCode, target}) => {
+    if (keyCode === 13) {
+        const { value } = target
+        const command = data.find(({command}) => command === value);
+        if (command && command.command === "clear") {
+            clearShell()
+            return
+        }
+        printCommand(command)
+    }
+}
+
+function clearShell() {
+    document.querySelectorAll(".output").forEach(elt => elt.remove())
+    clearInput()
+}
+
+function printCommand(command) {
+    const outputEl = document.createElement("div")
+
+    outputEl.className = "output"
+    outputEl.textContent = command ? command.output : "Cette commande n'existe pas."
+
+    outputsEl.insertAdjacentElement("beforeend", outputEl)
+
+    clearInput()
+}
+
+function clearInput() { inputEl.value = "" }
+
+
+/*
+
+À propos de moi
+
+
+Expérience professionnelle
+
+Parcours scolaire
+
+Projets / réalisations
+
+
+
+*/
