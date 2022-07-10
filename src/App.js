68637c9ab1b63e72af7077e5e6fd5f4ad06f3c3c
Geoffray Gaborit <gaborit.geoffray@icloud.com>
2022-07-10 16:38:51 +0200

    🔥

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h3 className="title">Under construction...</h3>
+      <a
+        href="https://github.com/Jofresh/portfolio"
+        target="_blank"
+        rel="noreferrer"
+        className="info"
+      >
+        See on Github
+      </a>
     </div>
   );
 }
